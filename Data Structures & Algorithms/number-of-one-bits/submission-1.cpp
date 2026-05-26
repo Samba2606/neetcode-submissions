c6204dedabe6305fda0fa139d27080cc2d647362
Samba2606 <b123146@iiit-bh.ac.in>
2026-05-26 09:43:46 +0530

    Add: number-of-one-bits - submission-1

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count =0;
+        // for(int i=31;i>=0;i--){
+        //     if(((n>>i)&1)==1){
+        //         count++;
+        //     }
+        // }
+        while(n>0){
+            n=n&(n-1);
+            count++;
+        }
+        return count;
+    }
+};
